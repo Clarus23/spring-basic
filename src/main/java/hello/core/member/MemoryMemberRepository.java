0d452698c8fb31807bbe4651c02bbb158a2d3538
Clarus23 <dnjs3023@gmail.com>
2025-10-17 13:44:18 +0900

    회원 도메인 설계 & 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository {
+//    private static Map<Long, Member> store = new HashMap<>();
+    // HashMap은 동시성 이슈가 발생 가능. 이런 경우 ConcurrentHashMap을 사용함.
+    private static Map<Long, Member> store = new ConcurrentHashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
